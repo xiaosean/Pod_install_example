92e3d7ccb54bbe68459c6a10f747475cd178c1c8
xiaosean <xiaosean5408@gmail.com>
2018-04-06 23:13:39 +0800

    first commit

---
+++ Pods/Target Support Files/Pods-test_Pod/Pods-test_Pod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_test_Pod : NSObject
+@end
+@implementation PodsDummy_Pods_test_Pod
+@end
---
+++ readme.md
@@ -0,0 +1,5 @@
+IOS套件管理-CocoaPods 教學
+
+可參考下面文章
+
+http://www.xiaosean.website/ios/2018/04/06/IOS-package-manager/
